885f3c72b00da509d2480ced568d9829d6fd1504
José Pedro <pedroosd28@gmail.com>
2022-05-11 19:43:57 -0300

    refactor: computers to reservas

--- app/actions/show_reservas.py
+++ app/actions/show_reservas.py
@@ -1,22 +1,22 @@
 from app.handles.ParserHandle import ParserAddress
 
 FILENAME = 'fixtures/RESERVAS.txt'
-COMPUTER_TEMPLATE = '{idx} - Nome={nome} | MAC={mac} | IP={ip}'
+RESERVA_TEMPLATE = '{idx} - Nome={nome} | MAC={mac} | IP={ip}'
 
 
 def show_reservas():
     print('Reservas: ')
     parser = ParserAddress(FILENAME)
-    computers = parser.parser()
+    reservas = parser.parser()
 
-    for idx, computer in enumerate(computers):
-        computer_str = COMPUTER_TEMPLATE.format(
+    for idx, reserva in enumerate(reservas):
+        reserva_str = RESERVA_TEMPLATE.format(
             idx=idx + 1,
-            nome=computer['computer_name'],
-            mac=computer['mac_address'],
-            ip=computer['ip_address'],
+            nome=reserva['computer_name'],
+            mac=reserva['mac_address'],
+            ip=reserva['ip_address'],
         )
 
-        print(computer_str, end='')
+        print(reserva_str, end='')
 
     return ""
