09f0643f8047e99cdbdb63d24ec806b0b64d27eb
José Pedro <pedroosd28@gmail.com>
2022-05-11 20:04:10 -0300

    feat: adding backup of dhcpd.conf function

--- app/actions/dhcp/start.py
+++ app/actions/dhcp/start.py
@@ -1,9 +1,14 @@
 import os
 from app import OUTPUT_SUCCESS_TEMPLATE
+from app.handles.DHCPHandler import DHCPHandler
 
 
 def start():
+    dhcp_handler = DHCPHandler()
+
     print("Criando backup do arquivo atual do DHCP")
+    dhcp_handler.create_backup_file()
+
     print("Gerando novo arquivo de configuração DHCP")
     print("Salvando configuração")
     print("Startando DHCP")
--- app/handles/DHCPHandler.py
+++ app/handles/DHCPHandler.py
@@ -1,2 +1,25 @@
+from app.handles.FileHandler import FileHandler
+from app.utils.get_today_date import get_today_date
+
+
 class DHCPHandler:
-    ...
+    DHCP_FILEPATH = "/etc/dhcp/dhcpd.conf"
+
+    def get_dhcp_conf(self):
+        try:
+            file_handler = FileHandler(self.DHCP_FILEPATH)
+            dhcp_file_content = file_handler.get_content(as_str=True)
+
+        except IndexError:
+            ...
+
+        return dhcp_file_content
+
+    def create_backup_file(self):
+        moment = get_today_date()
+        content = self.get_dhcp_conf()
+        filename = f"backup-{moment}.conf"
+
+        FileHandler.create_file(
+            filepath=f"fixtures/backups/{filename}", content=content
+        )
