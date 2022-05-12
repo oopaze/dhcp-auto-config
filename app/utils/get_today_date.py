c615404abc83ae91e2d2b85d8edfa13048f8041f
José Pedro <pedroosd28@gmail.com>
2022-05-12 00:25:25 -0300

    feat: adding date convertion comment

--- app/utils/get_today_date.py
+++ app/utils/get_today_date.py
@@ -2,4 +2,6 @@ from datetime import datetime
 
 
 def get_today_date():
-    return datetime.now().strftime("%d%m%Y%H%M%S")
+    return datetime.now().strftime(
+        "%d%m%Y%H%M%S"
+    )  # 12/05/2022 00:24:02 ficará 12052022002402
