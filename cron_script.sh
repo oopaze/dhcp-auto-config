74ad30cca173c72fdd7c10d5f403c94d51cb4cbc
José Pedro <pedroosd28@gmail.com>
2022-05-11 21:08:39 -0300

    fix: enable testing in cron script

--- cron_script.sh
+++ cron_script.sh
@@ -1,1 +1,2 @@
-@reboot root cd /home/oopaze/projects/faculdade/dhcp-auto-config && python3 run.py --cron=True
+# @reboot root cd /home/oopaze/projects/faculdade/dhcp-auto-config && python3 run.py --cron=True
+python3 run.py --cron=True
