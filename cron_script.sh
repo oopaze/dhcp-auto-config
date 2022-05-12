8236b45ac3a1536ac771067243138d13c116f873
José Pedro <pedroosd28@gmail.com>
2022-05-12 00:11:25 -0300

    refactor: removing sleep in cron script

--- cron_script.sh
+++ cron_script.sh
@@ -1,2 +1,2 @@
-# @reboot         root    (sleep 5; cd /home/foo/projects/faculdade/dhcp-auto-config && make run_cron)
+# @reboot         root    (cd /home/foo/projects/faculdade/dhcp-auto-config && make run_cron)
 python3 run.py --cron=True
